a371d9f1585c1c282e66f8078636b2eb2d064d4e
starwavelin <benjaminxianlin@gmail.com>
2021-01-27 08:51:16 -0500

    provide dummy book data to resolve ftn for rootQuery, testing on graphiQL passed

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,13 @@ const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+// dummy data
+const books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
@@ -27,6 +34,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLString } },
             resolve(parent, args) {
                 // code to get data from db / other source
+                return books.find(elem => elem.id === args.id);
             }
         }
     }
