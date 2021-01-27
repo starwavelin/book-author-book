b00621f1dd7ef25e6cee7528416703f3de6232bc
starwavelin <benjaminxianlin@gmail.com>
2021-01-26 22:10:40 -0500

    define /graphql endpoint using graphqlHTTP

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+
+}));
 
 app.listen(4001, () => {
     console.log('Listening on port 4001');
